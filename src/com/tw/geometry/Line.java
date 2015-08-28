c898c9e9f0ee4932b70199af3b6ef4e499c2ee67
priyadarshini <priyadr@thoughtworks.com>
2015-08-28 06:00:47 +0530

    length calculation for lines parallel to x axis

--- src/com/tw/geometry/Line.java
+++ src/com/tw/geometry/Line.java
@@ -1,10 +1,12 @@
 package com.tw.geometry;
 
+import static java.lang.Math.*;
+
 public class Line {
 
-    private double x1,y1,x2,y2;
+    private double x1, y1, x2, y2;
 
-    public Line(double x1,double y1,double x2,double y2) {
+    public Line(double x1, double y1, double x2, double y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = y2;
@@ -12,6 +14,8 @@ public class Line {
     }
 
     public double length() {
+        if(x1 == x2)
+            return abs(y2 - y1);
         return 0;
     }
 
--- test/com/tw/geometry/LineTest.java
+++ test/com/tw/geometry/LineTest.java
@@ -3,13 +3,19 @@ package com.tw.geometry;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static java.lang.Math.*;
+
 
 public class LineTest {
 
     @Test
     public void shouldHaveLengthZeroForLineWithSameCoordinates() {
-        assertEquals(0,new Line(0.0,0.0,0.0,0.0).length(),0.0);
+        assertEquals(0, new Line(0, 0, 0, 0).length(), 0.0);
     }
 
+    @Test
+    public void shouldHaveActualLengthLineParallelXAxis() {
+        assertEquals(1, new Line(1, 0, 1, 1).length(), 0.0);
+    }
 
 }
