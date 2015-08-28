2fa3239bdc04fa2f2350d3d21da9353701d5d03b
priyadarshini <priyadr@thoughtworks.com>
2015-08-28 07:18:37 +0530

    refactoring

--- src/com/tw/geometry/Line.java
+++ src/com/tw/geometry/Line.java
@@ -14,21 +14,31 @@ public class Line {
     }
 
     public double length() {
-        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+        return sqrt( squareOfDifference(x2, x1) + squareOfDifference(y2, y1) );
+    }
+
+    private double squareOfDifference(double value1, double value2) {
+        return pow(value2 - value1, 2);
+    }
+
+    private boolean comparePoints(double x1, double y1, double x2, double y2) {
+        if (x1 == x2 && y1 == y2)
+            return true;
+        return false;
     }
 
     @Override
     public boolean equals(Object that) {
         if(this == that)
             return true;
-        if(that == null)
-            return false;
-        Line thatLine = that instanceof Line ? (Line) that: null;
-        if(thatLine != null){
-            if(this.x1 == thatLine.x1 && this.x2 == thatLine.x2 && this.y1 == thatLine.y1 && this.y2 == thatLine.y2)
-                return true;
-            if(this.x1 == thatLine.x2 && this.x2 == thatLine.x1 && this.y1 == thatLine.y2 && this.y2 == thatLine.y1)
-                return true;
+        if(that != null) {
+            Line thatLine = that instanceof Line ? (Line) that : null;
+            if (thatLine != null) {
+                if (comparePoints(x1, y1, thatLine.x1 ,thatLine.y1) && comparePoints(x2, y2, thatLine.x2, thatLine.y2))
+                    return true;
+                if (comparePoints(x2, y2, thatLine.x1 ,thatLine.y1) && comparePoints(x1, y1, thatLine.x2, thatLine.y2))
+                    return true;
+            }
         }
         return false;
     }
