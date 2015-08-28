7cce23d0b2d5ac3656d94fb27ef633e7d7920679
priyadarshini <priyadr@thoughtworks.com>
2015-08-28 11:39:03 +0530

    Removing Point Construction out of Line Construction

--- src/com/tw/geometry/Line.java
+++ src/com/tw/geometry/Line.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Line {
     private Point p1, p2;
 
-    public Line(double x1, double y1, double x2, double y2) {
-        this.p1 = new Point(x1, y1);
-        this.p2 = new Point(x2, y2);
+    public Line(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
     public double length() {
--- test/com/tw/geometry/LineTest.java
+++ test/com/tw/geometry/LineTest.java
@@ -7,94 +7,94 @@ import static java.lang.Math.*;
 
 public class LineTest {
 
-    Line line1 = new Line(1, 1, 2, 2);
-    Line line2 = new Line(1, 1, 2, 2);
+    Line line1 = new Line(new Point(1, 1), new Point(2, 2));
+    Line line2 = new Line(new Point(1, 1), new Point(2, 2));
 
     @Test
     public void shouldHaveLengthZeroForLineWithSameCoordinates() {
-        Line line = new Line(0, 0, 0, 0);
+        Line line = new Line(new Point(0, 0), new Point(0, 0));
 
         assertEquals(0, line.length(), 0.0);
     }
 
     @Test
     public void shouldHaveActualLengthForLineParallelToXAxis() {
-        Line line = new Line(1, 0, 1, 1);
+        Line line = new Line(new Point(1, 0), new Point(1, 1));
 
         assertEquals(1, line.length(), 0.0);
     }
 
     @Test
     public void shouldHaveActualLengthForLineParallelToYAxis() {
-        Line line = new Line(1, 1, 2, 1);
+        Line line = new Line(new Point(1, 1), new Point(2, 1));
 
         assertEquals(1, line.length(), 0.0);
     }
 
     @Test
     public void shouldHaveActualLengthForLineNotParallelToXAndYAxis() {
-        Line line = new Line(1, 1, 2, 2);
+        Line line = new Line(new Point(1, 1), new Point(2, 2));
 
         assertEquals(sqrt(2), line.length(), 0.0);
     }
 
     @Test
     public void equalsItself() {
-        Line line = new Line(1, 1, 2, 2);
+        Line line = new Line(new Point(1, 1), new Point(2, 2));
 
         assertEquals(line, line);
     }
 
     @Test
     public void unequalsEmptyObject() {
-        Line line = new Line(1, 1, 2, 2);
+        Line line = new Line(new Point(1, 1), new Point(2, 2));
 
         assertNotEquals(line, null);
     }
 
     @Test
     public void unequalsInvalidObject() {
-        Line line = new Line(1, 1, 2, 2);
+        Line line = new Line(new Point(1, 1), new Point(2, 2));
 
         assertNotEquals(line, new Object());
     }
 
     @Test
     public void equalsLineWithSameXCoordinates() {
-        Line line1 = new Line(1, 1, 2, 2);
-        Line line2 = new Line(1, 1, 2, 2);
+        Line line1 = new Line(new Point(1, 1), new Point(2, 2));
+        Line line2 = new Line(new Point(1, 1), new Point(2, 2));
 
         assertEquals(line1, line2);
     }
 
     @Test
     public void equalsLineWithSameXCoordinatesWithOrderReversed() {
-        Line line1 = new Line(1, 1, 2, 2);
-        Line line2 = new Line(2, 2, 1, 1);
+        Line line1 = new Line(new Point(1, 1), new Point(2, 2));
+        Line line2 = new Line(new Point(2, 2), new Point(1, 1));
 
         assertEquals(line1, line2);
     }
 
     @Test
     public void equalsLineWithSameYCoordinates() {
-        Line line1 = new Line(1, 1, 2, 2);
-        Line line2 = new Line(2, 2, 1, 1);
+        Line line1 = new Line(new Point(1, 1), new Point(2, 2));
+        Line line2 = new Line(new Point(2, 2), new Point(1, 1));
 
         assertEquals(line1, line2);
     }
 
     @Test
     public void equalsLineWithSameYCoordinatesWithOrderReversed() {
-        Line line1 = new Line(1, 1, 2, 2);
-        Line line2 = new Line(2, 2, 1, 1);
+        Line line1 = new Line(new Point(1, 1), new Point(2, 2));
+        Line line2 = new Line(new Point(2, 2), new Point(1, 1));
 
         assertEquals(line1, line2);
     }
 
     @Test
     public void equalsLineWithSameYAndYCoordinates() {
-        Line line1 = new Line(1, 1, 2, 2);
-        Line line2 = new Line(2, 2, 1, 1);
+        Line line1 = new Line(new Point(1, 1), new Point(2, 2));
+        Line line2 = new Line(new Point(2, 2), new Point(1, 1));
 
         assertEquals(line1, line2);
     }
